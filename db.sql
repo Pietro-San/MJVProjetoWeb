454889e0b2e999fa391e8fe0f42606396be5a19f
Pietro <Pietro-San@users.noreply.github.com>
2018-07-02 16:24:55 -0300

    Add files via upload
    Esse foi o banco de dados que eu criei para usar no projeto. Ele foi feito em MySQL, o motivo de por que eu decidi usar duas tabelas relacionadas foi o fato de ao analisar o JSONPlaceholder, ter detectado que ele tem "userId", isso pra mim deixou claro a necessidade de uma tabela "user" para servir como autor dos textos que seriam colocados no "title" e "body".

---
+++ db.sql
@@ -0,0 +1,29 @@
+--Banco de dados: DB
+
+
+Create database db;
+
+use db;
+
+-- Estrutura da tabela `users`
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+COMMIT;
+
+-- Estrutura da tabela `posts`
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userId` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`),
+    CONSTRAINT userId_FK FOREIGN KEY (userId)
+    REFERENCES users(id)
+);
